b4d21836411af2d2c09b3444861b5dc640bb4da4
Leonardo Valdes <leon_valdes7591@yahoo.com>
2026-01-26 21:58:29 -0800

    implement insert and delete BST functions

--- BST.js
+++ BST.js
@@ -6,6 +6,18 @@ class TreeNode {
 		this.left = null;
 		this.right = null;
 	}
+
+	isLeafNode() {
+		return this.left === null && this.right === null;
+	}
+
+	hasOnlyRightChild() {
+		return this.left === null && this.right !== null;
+	}
+
+	hasOnlyLeftChild() {
+		return this.left !== null && this.right === null;
+	}
 }
 
 class Tree {
@@ -28,6 +40,63 @@ class Tree {
 
 		return root;
 	}
+
+	insert(value) {
+		this.root = this.insertRecursive(this.root, value);
+	}
+
+	insertRecursive(node, value) {
+		if (node === null) return new TreeNode(value);
+
+		if (value < node.data) {
+			node.left = this.insertRecursive(node.left, value);
+		} else if (value > node.data) {
+			node.right = this.insertRecursive(node.right, value);
+		} else if (value === node.data) {
+			console.log('value already exists in this node. no action taken');
+		}
+
+		return node;
+	}
+
+	delete(value) {
+		this.root = this.deleteRecursive(this.root, value);
+	}
+
+	deleteRecursive(node, value) {
+		// need to check if node to be deleted is a leaf, has one child or 2 children
+
+		// check if the tree even has a node or if weve reached the end without a match
+		if (node === null) {
+			return node;
+		}
+
+		// keep traversing if its not the matching value
+		if (node.data < value) {
+			node.right = this.deleteRecursive(node.right, value);
+		} else if (node.data > value) {
+			node.left = this.deleteRecursive(node.left, value);
+		} else {
+			// once theres a match, determine amount of child nodes
+			if (node.isLeafNode()) return null;
+			if (node.hasOnlyLeftChild()) return node.left;
+			if (node.hasOnlyRightChild()) return node.right;
+
+			// if we reach here, theres 2 child nodes. find the successor in the right subtree with lowest value
+			const successor = this.findSuccessor(node);
+			node.data = successor.data;
+			node.right = this.deleteRecursive(node.right, successor.data);
+		}
+		return node;
+	}
+
+	findSuccessor(node) {
+		let subTree = node.right;
+		while (subTree.left !== null) {
+			subTree = subTree.left;
+		}
+		return subTree;
+	}
 }
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
@@ -45,4 +114,15 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 
 // tests
 const myTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+myTree.insert(6345);
+prettyPrint(myTree.root);
+myTree.delete(0);
+prettyPrint(myTree.root);
+myTree.delete(6345);
+prettyPrint(myTree.root);
+myTree.delete(9);
+prettyPrint(myTree.root);
+myTree.delete(4);
+prettyPrint(myTree.root);
+myTree.delete(8);
 prettyPrint(myTree.root);
